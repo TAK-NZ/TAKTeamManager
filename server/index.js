dd444b8118f0d19f04c816058361631911095887
Christian Elsen <chris@chriselsen.net>
2026-07-31 15:45:33 +0000

    Allow Gravatar images in CSP img-src directive

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(helmet({
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'"],
       styleSrc: ["'self'", "'unsafe-inline'"],
-      imgSrc: ["'self'", "data:"],
+      imgSrc: ["'self'", "data:", "https://www.gravatar.com"],
       connectSrc: ["'self'"],
       fontSrc: ["'self'"],
       objectSrc: ["'none'"],
